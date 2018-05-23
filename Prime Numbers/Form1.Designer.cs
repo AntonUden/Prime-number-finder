7b68d8b026bf105880b0dddcc8d07e9c101c162f
BuildTools <udenanton@hotmail.com>
2018-05-23 09:42:07 +0200

    Initial commit

---
+++ Prime Numbers/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Prime_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run = new System.Windows.Forms.Button();
+            this.maxNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.results = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.run.Location = new System.Drawing.Point(12, 51);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(562, 23);
+            this.run.TabIndex = 0;
+            this.run.Text = "Check prime numbers";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // maxNumber
+            // 
+            this.maxNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maxNumber.Location = new System.Drawing.Point(12, 25);
+            this.maxNumber.Name = "maxNumber";
+            this.maxNumber.Size = new System.Drawing.Size(562, 20);
+            this.maxNumber.TabIndex = 1;
+            this.maxNumber.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Max number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progress
+            // 
+            this.progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progress.Location = new System.Drawing.Point(12, 80);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(562, 23);
+            this.progress.Step = 1;
+            this.progress.TabIndex = 3;
+            // 
+            // results
+            // 
+            this.results.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.results.Location = new System.Drawing.Point(12, 109);
+            this.results.Multiline = true;
+            this.results.Name = "results";
+            this.results.ReadOnly = true;
+            this.results.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.results.Size = new System.Drawing.Size(562, 201);
+            this.results.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 322);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxNumber);
+            this.Controls.Add(this.run);
+            this.MinimumSize = new System.Drawing.Size(602, 361);
+            this.Name = "Form1";
+            this.Text = "Prime Number Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.TextBox maxNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.TextBox results;
+    }
+}
+
